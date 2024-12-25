e86c65ce515f9d8660380efa8ef9a9d117e20f4c
Alejandro Aboy <aboyalejandro@gmail.com>
2024-12-25 13:40:24 +0100

    query update

--- db/queries.sql
+++ db/queries.sql
@@ -2,6 +2,7 @@ select
 	audio_id, 
 	transcript_id, 
 	audios.audio_key, 
+	audios.audio_type,
 	transcription->> 'note_title' as note_title,
 	transcription->> 'summary_text' as summary_text,
 	transcription->> 'transcript_text' as transcript_text

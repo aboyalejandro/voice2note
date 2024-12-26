1657dffb2bfb48319be156e941a833f1c94ab52d
Alejandro Aboy <aboyalejandro@gmail.com>
2024-12-26 12:39:29 +0100

    update lambda

--- lambdas/summarize/lambda_function.py
+++ lambdas/summarize/lambda_function.py
@@ -9,7 +9,7 @@ from utils import get_transcript, run_llm, export_summary, save_to_postgresql
 s3_client = boto3.client("s3")
 
 # ENVs AI
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 open_ai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 # ENVs Database
@@ -22,34 +22,63 @@ DB_PORT = os.getenv("DB_PORT")
 
 # PROMPTS
 gpt_rol = "You are a voice note summarizing assistant."
-gpt_prompt_summary = "You are a voice note summarizing assistant that provides summaries of no more than 2 sentences with simple language, here's your text"
-gpt_prompt_title = "You are a voice note summarizing assistant that provides titles for note summaries of no more than 3 words of simple language and output the result without quotes, here's your text"
+gpt_prompt_summary = """
+    You are a voice note summarizing assistant that provides summaries of no more than 2 sentences.
+    Please, the summary should talk to the user e.g. "You are planning a holiday..." instead of saying "The user is planning a holiday..."
+    Provide the output in the language of the text input, here's your text
+    """
+gpt_prompt_title = """
+    You are a voice note summarizing assistant that provides titles for note summaries of no more than 5 words, output the result without quotes. 
+    Provide the output in the language of the text input, here's your text
+    """
+
 
 # Lambda handler
-def lambda_handler(event, context): 
+def lambda_handler(event, context):
     try:
         # Extract bucket and object info from event
         bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
         object_key = event["Records"][0]["s3"]["object"]["key"]
-        
+
         print(f"New file detected: {object_key} in bucket {bucket_name}")
 
         transcript_text = get_transcript(bucket_name, object_key, s3_client)
-        summary_text = run_llm(transcript_text, gpt_prompt_summary, gpt_rol, open_ai_client)
-        note_title = run_llm(summary_text, gpt_prompt_title, gpt_rol, open_ai_client)
-        
-        audio_key = object_key.split('/')[-1].replace('.json', '')
-        user_id = audio_key.split('_')[0]
-
-        transcript_results = export_summary(bucket_name, user_id, audio_key, transcript_text ,summary_text, note_title, s3_client) 
-    
-        # Save transcript to PostgreSQL
-        transcript_query = (
-            "INSERT INTO transcripts (audio_key, s3_object_url, transcription, created_at) VALUES (%s, %s, %s, %s)"
+        summary_text = run_llm(
+            transcript_text, gpt_prompt_summary, gpt_rol, open_ai_client
         )
-        transcript_values = (audio_key, transcript_results['s3_object_url'], json.dumps(transcript_results), datetime.now())
-        save_to_postgresql(transcript_query, transcript_values, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT)
-    
+        note_title = run_llm(summary_text, gpt_prompt_title, gpt_rol, open_ai_client)
+
+        audio_key = object_key.split("/")[-1].replace(".json", "")
+        user_id = audio_key.split("_")[0]
+
+        transcript_results = export_summary(
+            bucket_name,
+            user_id,
+            audio_key,
+            transcript_text,
+            summary_text,
+            note_title,
+            s3_client,
+        )
+
+        # Save transcript to PostgreSQL
+        transcript_query = "INSERT INTO transcripts (audio_key, s3_object_url, transcription, created_at) VALUES (%s, %s, %s, %s)"
+        transcript_values = (
+            audio_key,
+            transcript_results["s3_object_url"],
+            json.dumps(transcript_results),
+            datetime.now(),
+        )
+        save_to_postgresql(
+            transcript_query,
+            transcript_values,
+            DB_HOST,
+            DB_NAME,
+            DB_USER,
+            DB_PASSWORD,
+            DB_PORT,
+        )
+
     except Exception as e:
         print(f"Error in processing: {e}")
         raise
--- lambdas/summarize/utils.py
+++ lambdas/summarize/utils.py
@@ -2,39 +2,42 @@ from datetime import datetime
 import json
 import psycopg2
 
+
 def get_transcript(bucket_name: str, object_key: str, s3_client):
     response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-    transcription_result = json.loads(response['Body'].read().decode('utf-8'))
-    transcript_text = transcription_result['results']['transcripts'][0]['transcript']
-    return [transcript_text,]
+    transcription_result = json.loads(response["Body"].read().decode("utf-8"))
+    transcript_text = transcription_result["results"]["transcripts"][0]["transcript"]
+    return transcript_text
+
 
 def run_llm(input: str, instruction: str, role: str, client):
-    prompt = f'{instruction}:\n\n{input}'
+    prompt = f"{instruction}:\n\n{input}"
     completion = client.chat.completions.create(
-          model="gpt-4", 
-          messages=[
-              {"role": "system", "content": f'{role}'},
-              {"role": "user", "content": prompt},
-          ],
-      )
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": f"{role}"},
+            {"role": "user", "content": prompt},
+        ],
+    )
     return completion.choices[0].message.content
+
 
 # Generate JSON output and upload to S3
 def export_summary(
     bucket_name: str,
-    user_id: str, 
-    audio_key: str, 
-    transcript_text:str , 
+    user_id: str,
+    audio_key: str,
+    transcript_text: str,
     summary_text: str,
     note_title: str,
-    s3_client
-    ):
+    s3_client,
+):
     try:
         timestamp = int(datetime.now().timestamp())
         json_file_name = f"{audio_key}_{timestamp}.json"
         json_file_path = f"/tmp/{json_file_name}"
-        output_file_path = 'transcripts/processed'
-        
+        output_file_path = "transcripts/processed"
+
         json_object = {
             "user_id": user_id,
             "s3_object_url": f"s3://{bucket_name}/{output_file_path}/{json_file_name}",
@@ -42,25 +45,26 @@ def export_summary(
             "transcript_text": transcript_text,
             "summary_text": summary_text,
         }
-        
+
         with open(json_file_path, "w") as json_file:
             json.dump(json_object, json_file)
-        s3_client.upload_file(json_file_path, bucket_name, f'{output_file_path}/{json_file_name}')
-        print(f"JSON file uploaded to {bucket_name}/{output_file_path}/{json_file_name}")
-        
+        s3_client.upload_file(
+            json_file_path, bucket_name, f"{output_file_path}/{json_file_name}"
+        )
+        print(
+            f"JSON file uploaded to {bucket_name}/{output_file_path}/{json_file_name}"
+        )
+
         return json_object
     except Exception as e:
         print(f"Error exporting summary: {e}")
         raise
 
+
 # Save to PostgreSQL
-def save_to_postgresql(query: str, 
-                       values: tuple, 
-                       host: str, 
-                       db: str, 
-                       user: str, 
-                       password: str, 
-                       port: str):
+def save_to_postgresql(
+    query: str, values: tuple, host: str, db: str, user: str, password: str, port: str
+):
     try:
         with psycopg2.connect(
             host=host, database=db, user=user, password=password, port=port

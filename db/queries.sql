2a0c701ecea95533066f84c8281d1e44aeaeb170
Alejandro Aboy <aboyalejandro@gmail.com>
2024-12-26 12:39:21 +0100

    add backend queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,11 +1,23 @@
-select 
-	audio_id, 
-	transcript_id, 
-	audios.audio_key, 
-	audios.audio_type,
-	transcription->> 'note_title' as note_title,
-	transcription->> 'summary_text' as summary_text,
-	transcription->> 'transcript_text' as transcript_text
-from audios
-left join transcripts 
-	on audios.audio_key = transcripts.audio_key 
+-- notes 
+SELECT 
+    audios.audio_key,
+    TO_CHAR(audios.created_at, 'MM/DD') as note_date,
+    COALESCE(transcription->>'note_title','Transcribing note...') as note_title,
+    COALESCE(transcription->>'summary_text','Your audio is being transcribed. It will show up in here when is finished.') as note_summary,
+FROM audios
+LEFT JOIN transcripts
+ON audios.audio_key = transcripts.audio_key
+WHERE user_id = %s
+ORDER BY audios.created_at DESC
+
+-- note_detail
+
+SELECT 
+    audios.audio_key,
+    TO_CHAR(audios.created_at, 'MM/DD') as note_date,
+    COALESCE(transcription->>'note_title','Transcribing note...') as note_title,
+    COALESCE(transcription->>'transcript_text','Your audio is being transcribed. It will show up in here when is finished.') as note_transcription
+FROM audios
+LEFT JOIN transcripts
+ON audios.audio_key = transcripts.audio_key
+WHERE audios.audio_key = %s

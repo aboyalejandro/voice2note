ea60762146437f9032102270bd03fea2f9a16527
Alejandro Aboy <aboyalejandro@gmail.com>
2024-12-27 13:17:16 +0100

    new ddl

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,37 +1,55 @@
 -- Users Table
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    hashed_password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE public.users (
+	user_id serial4 NOT NULL,
+	username varchar(255) NOT NULL,
+	hashed_password varchar(255) NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	reset_token text NULL,
+	reset_token_expires timestamptz NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (user_id)
 );
+CREATE INDEX idx_users_user_id ON public.users USING btree (user_id);
+
+-- Sessions Table
+CREATE TABLE public.sessions (
+	session_id text NOT NULL,
+	user_id int4 NULL,
+	created_at timestamptz DEFAULT CURRENT_TIMESTAMP NULL,
+	expires_at timestamptz NULL,
+	deleted_at timestamptz NULL,
+	CONSTRAINT sessions_pkey PRIMARY KEY (session_id)
+);
+CREATE INDEX idx_sessions_user_active ON public.sessions USING btree (user_id, deleted_at) WHERE (deleted_at IS NULL);
+ALTER TABLE public.sessions ADD CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id);
 
 -- Audios Table
-CREATE TABLE audios (
-    audio_id SERIAL PRIMARY KEY,
-    audio_key VARCHAR(15) NOT NULL UNIQUE, 
-    user_id INTEGER NOT NULL,
-    s3_object_url TEXT NOT NULL,
-    audio_type VARCHAR(8) NOT NULL CHECK (audio_type IN ('recorded', 'uploaded')),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    metadata JSONB,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE user_{user_id}.audios (
+	audio_id serial4 NOT NULL,
+	audio_key varchar(15) NOT NULL,
+	user_id int4 NOT NULL,
+	s3_object_url text NOT NULL,
+	audio_type varchar(8) NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	deleted_at timestamp NULL,
+	metadata jsonb NULL,
+	CONSTRAINT audios_audio_key_key UNIQUE (audio_key),
+	CONSTRAINT audios_audio_type_check CHECK (((audio_type)::text = ANY ((ARRAY['recorded'::character varying, 'uploaded'::character varying])::text[]))),
+	CONSTRAINT audios_pkey PRIMARY KEY (audio_id)
 );
+CREATE INDEX idx_audios_audio_id ON user_{user_id}.audios USING btree (audio_id);
+ALTER TABLE user_{user_id}.audios ADD CONSTRAINT audios_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_{user_id}.users(user_id);
+ALTER TABLE user_{user_id}.audios ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user_{user_id}.users(user_id);
 
 -- Transcripts Table
-CREATE TABLE transcripts (
-    transcript_id SERIAL PRIMARY KEY,
-    audio_key VARCHAR(255) NOT NULL,  
-    s3_object_url TEXT,
-    transcription JSONB,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    FOREIGN KEY (audio_key) REFERENCES audios(audio_key)
+CREATE TABLE user_{user_id}.transcripts (
+	transcript_id serial4 NOT NULL,
+	audio_key varchar(255) NOT NULL,
+	s3_object_url text NULL,
+	transcription jsonb NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	deleted_at timestamp NULL,
+	CONSTRAINT transcripts_pkey PRIMARY KEY (transcript_id)
 );
-
--- Explicitly create an index for the primary key
-CREATE INDEX idx_users_user_id ON users(user_id);
-CREATE INDEX idx_audios_audio_id ON audios(audio_id);
-CREATE INDEX idx_transcripts_transcript_id ON transcripts(transcript_id);
-CREATE INDEX idx_transcripts_audio_id ON transcripts(audio_key);
+CREATE INDEX idx_transcripts_audio_id ON user_{user_id}.transcripts USING btree (audio_key);
+CREATE INDEX idx_transcripts_transcript_id ON user_{user_id}.transcripts USING btree (transcript_id);
+ALTER TABLE user_{user_id}.transcripts ADD CONSTRAINT transcripts_audio_key_fkey FOREIGN KEY (audio_key) REFERENCES user_{user_id}.audios(audio_key);
